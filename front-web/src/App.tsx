72b33d7d78f3cfc7fd8fae42ce2a37119df50a20
Elias Borges <eliasborges@unipam.edu.br>
2021-01-07 11:22:39 -0300

    Add HomePage

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import Home from './Home';
 import NavBar from './Navbar';
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
