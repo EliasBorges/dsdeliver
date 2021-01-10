eda960139963209686d38b36f5c42d055d76519d
Elias Borges <eliasborges@unipam.edu.br>
2021-01-09 23:24:57 -0300

    Add cards dinamics

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,13 +8,13 @@ function Footer() {
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="youtube" target="_blank">
+                <a href="youtube">
                     <YoutubeImage />
                 </a>
-                <a href="https://www.linkedin.com/in/eliasborges/" target="_blank">
+                <a href="https://www.linkedin.com/in/eliasborges/">
                     <LinkedinImage />
                 </a>
-                <a href="https://www.instagram.com/eliasjborges/" target="_blank">
+                <a href="https://www.instagram.com/eliasjborges/">
                     <InstagramImage />
                 </a>
             </div>
